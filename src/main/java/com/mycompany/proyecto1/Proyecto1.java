95710e0eb036b2f94cf8d8edf903185fa582afa1
fran9 <fran9@Fran>
2025-04-20 13:01:16 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author fran9
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Primera versión Git");
+        System.out.println("Segunda versión Git");
+    }
+}
